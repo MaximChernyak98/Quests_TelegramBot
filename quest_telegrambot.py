04f4639badfae8ca865cb5386c4af11277a6a338
mecher_l_home <maximchernyak98@gmail.com>
2020-08-22 11:28:59 +0300

    try to take number of bot from txt

--- quest_telegrambot.py
+++ quest_telegrambot.py
@@ -15,12 +15,16 @@ credentials = Credentials.from_service_account_file(
     CREDENTIALS_FILE, scopes=scope)
 gc = gspread.authorize(credentials)
 
+# Открываем файл с номером бота
+with open('Bot_number.txt', 'r') as txt:
+    bot_number = txt.readlines()
+
 # Открываем таблицу
 worksheet = gc.open_by_key(
     '1UCAw3PTluEKc3noSBrMcLlfCU1WHhpraLd8Zwh9z9Lg').sheet1
 
 # Открываем бота
-bot = telebot.TeleBot("1134602259:AAFnxbhTUG3WQlVBjzdH_11zRywl9lYK1_4")
+bot = telebot.TeleBot(str(bot_number[0]))
 
 
 # Отправка списка квестов
@@ -41,66 +45,67 @@ def print_quests():
 
 
 def run_print():
-    time_start = (datetime.now() + timedelta(seconds=5)
-                  ).strftime("%H:%M:%S")
+    time_start = (datetime.now() + timedelta(seconds=5)).strftime("%H:%M:%S")
     schedule.every().day.at(time_start).do(print_quests)
     # schedule.every(20).seconds.do(print_quests)
+    # schedule.every().day.at("08:40:00").do(print_quests)
     while True:
         schedule.run_pending()
         time.sleep(2)
 
 
 def run_query():
+    while True:
+        # Обработчик кнопок-ответов
+        @bot.callback_query_handler(func=lambda call: True)
+        def query_handler(call):
+            # Получаем информация от нажатой кнопки
+            state = call.data.split("/")[0]
+            day = call.data.split("/")[1]
+            number_of_quest = int(call.data.split("/")[2])+1
+            # Ищем строку с нужной датой
+            date_list = [str(i) for i in worksheet.col_values(1)]
+            row = int(date_list.index(day)) + 1
+            if state == 'успех':
+                bot.answer_callback_query(
+                    callback_query_id=call.id, text=f'Молодца {number_of_quest} {day} {row}')
+                set_cell_0_or_1(row, number_of_quest, 1)
 
-    # Обработчик кнопок-ответов
-    @bot.callback_query_handler(func=lambda call: True)
-    def query_handler(call):
-        # Получаем информация от нажатой кнопки
-        state = call.data.split("/")[0]
-        day = call.data.split("/")[1]
-        number_of_quest = int(call.data.split("/")[2])+1
-        # Ищем строку с нужной датой
-        date_list = [str(i) for i in worksheet.col_values(1)]
-        row = int(date_list.index(day)) + 1
-        if state == 'успех':
-            bot.answer_callback_query(
-                callback_query_id=call.id, text=f'Молодца {number_of_quest} {day} {row}')
-            set_cell_0_or_1(row, number_of_quest, 1)
+            else:
+                bot.answer_callback_query(
+                    callback_query_id=call.id, text=f'Слабочок {number_of_quest} {day} {row}')
+                set_cell_0_or_1(row, number_of_quest, 0)
 
-        else:
-            bot.answer_callback_query(
-                callback_query_id=call.id, text=f'Слабочок {number_of_quest} {day} {row}')
-            set_cell_0_or_1(row, number_of_quest, 0)
+        def set_cell_0_or_1(row, column, state):
+            cell = f'{colnum_string(column)}' + f'{row}'
+            if state == 0:
+                worksheet.update_cell(row, column, 0)
+                # красный
+                worksheet.format(cell,
+                                 {'backgroundColor': {
+                                     "red": 22,
+                                     "green": 103,
+                                     "blue": 103
+                                 }})
+            else:
+                worksheet.update_cell(row, column, 1)
+                # зеленый
+                worksheet.format(cell,
+                                 {'backgroundColor': {
+                                     "red": 74,
+                                     "green": 41,
+                                     "blue": 88
+                                 }})
 
-    def set_cell_0_or_1(row, column, state):
-        cell = f'{colnum_string(column)}' + f'{row}'
-        if state == 0:
-            worksheet.update_cell(row, column, 0)
-            # красный
-            worksheet.format(cell,
-                             {'backgroundColor': {
-                                 "red": 22,
-                                 "green": 103,
-                                 "blue": 103
-                             }})
-        else:
-            worksheet.update_cell(row, column, 1)
-            # зеленый
-            worksheet.format(cell,
-                             {'backgroundColor': {
-                                 "red": 74,
-                                 "green": 41,
-                                 "blue": 88
-                             }})
+        def colnum_string(n):
+            string = ""
+            while n > 0:
+                n, remainder = divmod(n - 1, 26)
+                string = chr(65 + remainder) + string
+            return string
 
-    def colnum_string(n):
-        string = ""
-        while n > 0:
-            n, remainder = divmod(n - 1, 26)
-            string = chr(65 + remainder) + string
-        return string
-
-    bot.polling(none_stop=True)
+        bot.polling(none_stop=True)
+        time.sleep(3)
 
 
 # Запуск потока под расписание
