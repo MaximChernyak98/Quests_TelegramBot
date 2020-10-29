fe3899e69c27d1584cb87f3a6c86eb31527766db
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-10-29 12:11:26 +0300

    relocate polling for stop RAM leakeage

--- quest_telegrambot.py
+++ quest_telegrambot.py
@@ -22,13 +22,12 @@ logging.basicConfig(
 def main():
     dp = settings.MYBOT.dispatcher
     jq = settings.MYBOT.job_queue
-    time_to_start = datetime.time(hour=7, minute=30)
+    time_to_start = datetime.time(hour=9, minute=20)
     jq.run_daily(utils.print_quests, time=time_to_start)
-
+    settings.MYBOT.start_polling()
     while True:
         dp.add_handler(CallbackQueryHandler(handlers.get_state_of_quest))
         time.sleep(5)
-        settings.MYBOT.start_polling()
 
 
 if __name__ == '__main__':
