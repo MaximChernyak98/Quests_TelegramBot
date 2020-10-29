de1c7a701712565bce89e20e3ec18e4aa1150753
Максим Черняк _ Win_laptop <maximchernyak98@gmail.com>
2020-10-29 10:19:07 +0300

    add sleep

--- quest_telegrambot.py
+++ quest_telegrambot.py
@@ -22,11 +22,12 @@ logging.basicConfig(
 def main():
     dp = settings.MYBOT.dispatcher
     jq = settings.MYBOT.job_queue
-    time_to_start = datetime.time(hour=19, minute=53)
+    time_to_start = datetime.time(hour=7, minute=30)
     jq.run_daily(utils.print_quests, time=time_to_start)
 
     while True:
         dp.add_handler(CallbackQueryHandler(handlers.get_state_of_quest))
+        time.sleep(5)
         settings.MYBOT.start_polling()
 
 
